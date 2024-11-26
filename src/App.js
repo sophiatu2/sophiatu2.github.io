5cd47634e48a6a6ced4709307559040a2f3c4b9c
Sophia <sophiatu217@gmail.com>
2024-11-26 10:28:37 -0800

    Added Navbar

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
 import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
 
-import MyClassComponent from "./components/MyClassComponent";
-import MyFunctionalComponent from "./components/MyFunctionalComponent";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <BrowserRouter>
-      <h1>This is a React App</h1>
-      <p>This is a paragraph</p>
-      <Switch>
-        <Route exact path="/class" component={MyClassComponent} />
-        <Route
-          exact
-          path="/func"
-          component={MyFunctionalComponent}
-        />
-      </Switch>
-    </BrowserRouter>
+    <div className="App">
+      <Navbar></Navbar>
+    </div>
   );
 }
 
