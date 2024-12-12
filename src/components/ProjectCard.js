4dabe53beee32b3d0c0d70b4bab01c4a91bb987c
Sophia <sophiatu217@gmail.com>
2024-12-12 10:02:06 -0500

    Made ProjectCard clickable

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -3,20 +3,22 @@ import { AiFillGithub } from "react-icons/ai";
 
 function Card({ title, img, link, desc, skills, other }) {
   return (
-    <div className="project-card">
-      <img src={img} alt={"Project screenshot"} />
-      <div className="project-card-content">
-        <h3>{title}</h3>
-        {desc}
-        <p>
-          <b>Skills:</b> {skills}
-        </p>
-        <button className="custom-button" href={link} target="_blank">
-          <AiFillGithub /> See on github
-        </button>
-        {other}
+    <a className="link-wrapper" href={link}>
+      <div className="project-card">
+        <img src={img} alt={"Project screenshot"} />
+        <div className="project-card-content">
+          <h3>{title}</h3>
+          {desc}
+          <p>
+            <b>Skills:</b> {skills}
+          </p>
+          <button className="custom-button" href={link} target="_blank">
+            <AiFillGithub /> See on github
+          </button>
+          {other}
+        </div>
       </div>
-    </div>
+    </a>
   );
 }
 
