c2420312ea72ec3e180acabd8502325be7c3b620
Sophia <sophiatu217@gmail.com>
2024-12-20 12:45:11 -0500

    Fixed github links

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -139,32 +139,59 @@ function Projects() {
         <ProjectCard
           title="Responsive Redesign"
           img={redesignScreenshot}
-          link="https://vibingcat217.github.io/responsive-redesign/"
+          link="https://github.com/sophiatu2/CS1300-Responsive-Redesign"
           desc={
             "A website redesign for Johnny Burrito, a place I frequented during my investment banking internship in Charlotte. I removed unnecessary tabs and redundant links and addressed overall usability and accessibility issue."
           }
           skills={"React, Figma, Balsamiq, UI/UX"}
+          other={
+            <button
+              className="custom-button"
+              href={"https://vibingcat217.github.io/responsive-redesign/"}
+              target="_blank"
+            >
+              View website
+            </button>
+          }
         />
 
         <ProjectCard
           title="Iterative Design"
           img={iterativeScreenshot}
-          link="https://jumpylemur431.github.io/iterative-design/"
+          link="https://github.com/jumpylemur431/iterative-design"
           desc={
             "Interactive interface design for Discz, a music discovery social media platform from Y Combinator. I worked in a group to prototype the app, making sure to align with the original mission and taking inspriation from Tinder and other music apps. We also received critiques on our design and assembled a list of potential changes based on the feedback."
           }
           skills={"Figma, Balsamiq, Product Design"}
+          other={
+            <button
+              className="custom-button"
+              href={"https://jumpylemur431.github.io/iterative-design/"}
+              target="_blank"
+            >
+              View website
+            </button>
+          }
         />
 
         <ProjectCard
           title="Development"
           img={developmentScreenshot}
           caption="Rainbow Six Siege Shop"
-          link="https://vibingcat217.github.io/development/"
+          link="https://github.com/sophiatu2/development"
           desc={
             "An interface mirroring a shopping system that allows Rainbow Six Siege players to calculate how much Renown (in-game currency) they need to purchase their desired operators. Players can filter by various metrics and optimize their Renown spending by carefully choosing which combination of characters they would like to purchase and working towards the total cost."
           }
           skills={"React"}
+          other={
+            <button
+              className="custom-button"
+              href={"https://sophiatu2.github.io/development/"}
+              target="_blank"
+            >
+              View website
+            </button>
+          }
         />
       </Container>
     </div>
